9116b19b927710db17e6f6548448839b9b88d24d
Tirvy <8tirvy@gmail.com>
2017-01-20 10:50:27 +0300

    Photographers list is viewed

--- scripts/test/test_controllers.js
+++ scripts/test/test_controllers.js
@@ -2,19 +2,20 @@
     angular.module('test.controllers', ['test.services', 'test.directives'])
         .controller('TestCtrl', ['$scope', '$rootScope', '$location', 'TestService',
             function ($scope, $rootScope, $location, testService) {
-                $scope.test = 'HI';
-
-                $scope.arr = [];
-
-                $scope.clickThis = function (val) {
-                    $scope.test = val;
-                };
-                testService.sayHello();
-                for (var i = 0; i < 100; i++) {
-                    $scope.arr.push(i);
-                }
+                // $scope.test = 'HI';
+                //
+                // $scope.arr = [];
+                //
+                // $scope.clickThis = function (val) {
+                //     $scope.test = val;
+                // };
+                // testService.sayHello();
+                // for (var i = 0; i < 100; i++) {
+                //     $scope.arr.push(i);
+                // }
                 testService.getPhotographers().then(function (res) {
                     console.log(res);
+                    $scope.photographersList = res.data;
                 })
             }])
 })();
--- scripts/test/test_services.js
+++ scripts/test/test_services.js
@@ -2,9 +2,9 @@
     angular.module('test.services', [])
         .service('TestService', ['$http','$q', function ($http, $q) {
             var self = this;
-            self.sayHello = function () {
-              console.log('Hello');
-            };
+            // self.sayHello = function () {
+            //   console.log('Hello');
+            // };
             self.getPhotographers = function () {
                 return $http.get('https://jsonplaceholder.typicode.com/users');
             };
